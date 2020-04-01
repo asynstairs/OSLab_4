33eb0f1d06f945f23475f53d897636a143a193e5
gitlng <61177105+gitlng@users.noreply.github.com>
2020-04-01 14:57:24 +0300

    os_lab4_linux.c

---
+++ os_lab4_linux.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main ( void ){
+    pid_t p;
+    int exitcode;
+    printf("Original PID = %d\n", p);
+
+    switch ( p = fork() ) {
+        case 0: {
+            printf("Child Process. PID = %d, PPID = %d\n", getpid(), getppid());
+            exit(exitcode);
+        }
+        case -1: {
+            printf("Error\n");
+        }
+        default: {
+            printf("Parent Process. PID = %d, Child PID = %d\n", getpid(), p);
+            wait(&exitcode);
+            if ( WIFEXITED (exitcode) ){
+                printf("exit() successful in child process.\n");
+                printf("exitcode = %d\n", WEXITSTATUS(exitcode));
+            }
+        }
+    }
+}
